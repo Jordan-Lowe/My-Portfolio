75d220a98f0a2bc61cb6d154865a1870fdd6293e
Jordan Lowe <73576588+Jordan-Lowe@users.noreply.github.com>
2023-10-16 13:11:51 +1300

    Initial commit

---
+++ server/server.ts
@@ -0,0 +1,13 @@
+// .server/server.ts
+import express from 'express'
+import path from 'path'
+
+const app = express()
+const PORT = 5173
+
+// Assuming your public folder is at the root level of your project
+app.use(express.static(path.join(__dirname, '../public')))
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
