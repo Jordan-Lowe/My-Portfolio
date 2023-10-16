972fb20245249b449e81608cfd27a9ace5bc04c8
Jordan Lowe <jordanlowe659@gmail.com>
2023-10-16 14:10:00 +1300

    Basic Setup

--- server/server.ts
+++ server/server.ts
@@ -8,6 +8,8 @@ const PORT = 5173
 // Assuming your public folder is at the root level of your project
 app.use(express.static(path.join(__dirname, '../public')))
 
+app.use('/dist', express.static(path.join(__dirname, '../dist')))
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
 })
